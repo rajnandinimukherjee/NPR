86af4cebc786ca6674a18eccd2e6e31f2cf90587
Rajnandini Mukherjee <r.mukherjee@soton.ac.uk>
2023-02-01 15:56:39 +0000

    added fourquark_analysis class

--- NPR_classes.py
+++ NPR_classes.py
@@ -155,3 +155,89 @@ class bilinear_analysis:
             plt.savefig('plots/'+self.ens+'_action_comp_bl.pdf')
             print('Plot saved to plots/'+self.ens+'_action_comp_bl.pdf')
 
+class fourquark_analysis:
+    def __init__(self, ensemble, loadpath=None, **kwargs):
+
+        self.ens = ensemble
+        info = params[self.ens]
+        self.sea_mass = '{:.4f}'.format(info['masses'][0])
+        self.non_sea_masses = ['{:.4f}'.format(info['masses'][k])
+                               for k in range(1,len(info['masses']))]
+
+        self.actions = [info['gauges'][0]+'_'+info['baseactions'][0],
+                   info['gauges'][1]+'_'+info['baseactions'][1]]
+
+        if loadpath==None:
+            self.momenta, self.avg_results, self.avg_errs = {}, {}, {}
+            for data in [self.momenta, self.avg_results, self.avg_errs]:
+                for a1, a2 in itertools.product([0,1],[0,1]):
+                    data[(a1,a2)] = {}
+
+        else:
+            print('Loading NPR bilinear data from '+loadpath)
+            self.momenta, self.avg_results, self.avg_errs = pickle.load(
+                                                            open(loadpath, 'rb')) 
+            self.all_masses = list(self.momenta[(0,0)].keys()) 
+
+    
+    def NPR(self, masses, action=(0,0), scheme=1, **kwargs): 
+        m1, m2 = masses
+        a1, a2 = action
+        a1, a2 = self.actions[a1], self.actions[a2]
+        
+        self.data = path+self.ens
+        if not os.path.isdir(self.data):
+            print('NPR data for this ensemble could not be found on this machine')
+        else:
+            self.fq_list = common_cf_files(self.data,
+                                           'fourquarks', prefix='fourquarks_')
+
+            results = {}
+            errs = {}
+
+            for f in tqdm(range(len(self.fq_list)), leave=False):
+                prop1_name, prop2_name = self.fq_list[f].split('__')
+                prop1_info, prop2_info = decode_prop(prop1_name), decode_prop(prop2_name)
+
+                condition1 = (prop1_info['am']==m1 and prop2_info['am']==m2)
+                condition2 = (prop1_info['prop']==a1 and prop2_info['prop']==a2) 
+
+                if (condition1 and condition2):
+                    prop1 = external(self.ens, filename=prop1_name)
+                    prop2 = external(self.ens, filename=prop2_name)
+                    mom_diff = prop1.tot_mom-prop2.tot_mom
+
+                    condition3 = prop1.mom_sq==prop2.mom_sq
+                    condition4 = prop1.mom_sq==scheme*np.linalg.norm(mom_diff)**2
+
+                    if (condition3 and condition4):
+                        fq = fourquark(self.ens, prop1, prop2)
+                        fq.errs()
+                        if fq.q not in results.keys():
+                            results[fq.q] = fq.Z_V
+                            errs[fq.q] = fq.Z_V_errs
+
+                self.momenta[action][(m1,m2)] = sorted(results.keys())
+                self.avg_results[action][(m1,m2)] = np.array([results[mom]
+                                for mom in self.momenta[action][(m1,m2)]])
+                self.avg_errs[action][(m1,m2)] = np.array([errs[mom]
+                                for mom in self.momenta[action][(m1,m2)]])
+    
+    def save_NPR(self, addl_txt='', **kwargs):
+        filename = 'pickles/'+self.ens+'_fq.p'
+        pickle.dump([self.momenta, self.avg_results, self.avg_errs],
+                    open(filename,'wb'))
+        print('Saved fourquark NPR results to '+filename)
+
+    def NPR_all(self, massive=False, save=True, **kwargs):
+        for a1,a2 in itertools.product([0,1],[0,1]):
+            self.NPR((self.sea_mass, self.sea_mass), action=(a1,a2))
+        if massive:
+            for mass in self.non_sea_masses:
+                self.NPR((self.sea_mass, mass))
+                self.NPR((mass, self.sea_mass))
+                self.NPR((mass, mass))
+
+        if save:
+            self.save_NPR()
+    
